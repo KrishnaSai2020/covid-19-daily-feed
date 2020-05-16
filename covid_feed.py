58764a3c9172cf4e03f322390bcf62561e4274e9
Fizzes27! <krishnasai.chivukula@gmail.com>
2020-05-16 12:39:27 +0100

    intial commit

--- covid_feed.py
+++ covid_feed.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 
 def request_api_data():
@@ -6,8 +7,18 @@ def request_api_data():
     res = requests.get(url)
     if res.status_code != 200:
         raise RuntimeError(f'error fetching: {res.status_code}, check the api and try again ')
-    return res
+    response = res.json()
+    return response
 
 
 response = request_api_data()
-print(response.text)
+dict_of_case_nums = {
+    'date' : [],
+    'cases' : []
+}
+for dict in response:
+    dict_of_case_nums['cases'].append(dict['Cases'])
+    dict_of_case_nums['date'].append(dict['Date'])
+
+df = pd.DataFrame(dict_of_case_nums, columns=['date', 'cases'])
+print(df)
